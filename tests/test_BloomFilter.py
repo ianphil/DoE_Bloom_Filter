ac73463068b14bb943b05252385ca44526168c8b
Ian Philpot <ian.philpot@microsoft.com>
2019-03-05 13:05:14 -0500

    Data tests

---
+++ tests/test_BloomFilter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from bloom_filter.bloom_filter import BloomFilter
+from bloom_filter.users import PresentUsers, AbsentUsers
+
+class BloomFilter_TestSuite(unittest.TestCase):
+    """Tests for Absent Users class"""
+    def __init__(self):
+        self.present_users = PresentUsers('./bloom_filter/resources/present.txt')
+        self.absent_users = AbsentUsers('./bloom_filter/resources/absent.txt')
+
+    def test_add_bloomfilter(self):
+        bf = BloomFilter(124, 4)
+        for i in range(20):
+            bf += self.present_users[i]
+        assert len(bf) == 20
+        
