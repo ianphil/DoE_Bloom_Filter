9586ab961c3a578ecf4d284cda64139398f77c5f
Ian Philpot <ian.philpot@microsoft.com>
2019-03-05 13:20:52 -0500

    Tests for bloom filter class started

--- tests/test_BloomFilter.py
+++ tests/test_BloomFilter.py
@@ -6,14 +6,16 @@ from bloom_filter.bloom_filter import BloomFilter
 from bloom_filter.users import PresentUsers, AbsentUsers
 
 class BloomFilter_TestSuite(unittest.TestCase):
-    """Tests for Absent Users class"""
-    def __init__(self):
+    """Tests for Bloom Filter class"""
+
+    @classmethod
+    def setUpClass(self):
         self.present_users = PresentUsers('./bloom_filter/resources/present.txt')
         self.absent_users = AbsentUsers('./bloom_filter/resources/absent.txt')
 
     def test_add_bloomfilter(self):
         bf = BloomFilter(124, 4)
         for i in range(20):
-            bf += self.present_users[i]
-        assert len(bf) == 20
+            bf.add(self.present_users[i])
+        assert bf.bit_array.count() == 58
         
