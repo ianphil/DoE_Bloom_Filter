a4a1bcec9acefe8ffc8a2fe32b4491fce8c994d3
Ian Philpot <ian.philpot@microsoft.com>
2019-03-07 08:54:15 -0800

    added comments for calcs

--- src/01_doe_calculate_sets.py
+++ src/01_doe_calculate_sets.py
@@ -2,23 +2,33 @@
 
 from model.bloom_filter import BloomFilterCalculator
 
-def test_false_positive_probability():
-    calc = BloomFilterCalculator()
-    p = calc.false_positive_probability(124, 4, 20)
-
+# These are the calculations to understand the right-size of the 
+# factors that are controlable for Bloom Filters. We are given a number 
+# of items and an acceptable false positive probability. Using these 
+# factors, we are able to calculate the size of the bit array and the 
+# amount of hash passes needed to optimize and balance the factors 
+# against each other.
 def main():
     calc = BloomFilterCalculator()
 
+    # Number of items added to the Bloom Filter
     n = 10000
+
+    # Probability of false positives
     p = 0.05
 
+    # Size of bit array that will provide storage for the hashes
     m = calc.bit_array_size(n, p)
     msg = 'Bit Array size for 10,000 items with a 5% false positive probability: {}'.format(m)
     print(msg)
 
+    # The amount of times we will hash the item
     k = calc.hash_pass_count(m, n)
     msg = 'Hash passes for 10,000 items with a {} sized bit array: {}'.format(m, k)
     print(msg)
 
 if __name__ == '__main__':
     main()
+
+
+# TODO: Slide to define m,n,p,k and formulas  
