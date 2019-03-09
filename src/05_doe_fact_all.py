5ee916dabdb363fa1d770ee5f060e1db857d4f24
Ian Philpot <ian.philpot@microsoft.com>
2019-03-09 11:24:44 -0500

    Started x*2 exp

--- src/05_doe_fact_all.py
+++ src/05_doe_fact_all.py
@@ -2,6 +2,7 @@
 
 from model.bloom_filter import BloomFilter
 from model.users import PresentUsers, AbsentUsers
+import matplotlib.pyplot as plt 
 
 # This is the last experiment, a 2 factorial designed experiment. The goal is to understand 
 # the mass effect of each variable, but also the interaction between the to variables we control
@@ -23,6 +24,9 @@ def main():
     # a step of 10,000. This should surround the right sized value of 62352. k range is 3 to
     # 5 and also should surround the right sized value of 4.
     # TODO: O(n^2) - refactor to more be efficient using a memoization pattern.
+    cnt_size = []
+    cnt_passes = []
+    cnt_fp = []
     for hash_count in range(3, 5):
         for bit_arr_size in range(50000, 70000, 10000):
 
@@ -40,8 +44,16 @@ def main():
                 if user in bloom_filter:
                     false_positive_count += 1
 
+            cnt_fp.append(false_positive_count)
+            cnt_passes.append(hash_count)
+            cnt_size.append(bit_arr_size)
+
             print('There are {} false positives when bit array size is {} and hash count is {}'
                 .format(false_positive_count, bit_arr_size, hash_count))
+
+    # plt.plot(cnt_passes, cnt_fp)   
+    plt.plot(cnt_size, cnt_fp)
+    plt.show() 
 
 if __name__ == '__main__':
     main()
